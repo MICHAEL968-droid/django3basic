b5c11cf3bd7c6aba7cafb7fcb4b5ec11222fcd9c
RAJEEV <rajeevkushwaha1001@gmail.com>
2020-12-27 06:07:56 -0500

    first tempatates editing

--- rajeev/urls.py
+++ rajeev/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views 
+
 
 urlpatterns = [
-    path('',include('firstapp.urls')),
+    path('ty/',include('firstapp.urls')),
+    path('',views.home,name='home'),
     path('admin/', admin.site.urls),
 ]
